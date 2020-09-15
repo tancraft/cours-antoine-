bfe1bd78f5953d3cc4f7020e197f65e04ec5b5f8
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-09-15 12:28:48 +0200

    ajout4.5

---
+++ php apprendre/premierphp/exercices/exercices4/exercice4.5.php
@@ -0,0 +1,22 @@
+<?php
+
+// on demande le sexe a l'utilisateur
+  $sexe = readline ("merci d'indiquer votre sexe (tapez m ou f): ");
+
+// on test quand meme que la valeur entrée est correcte sinon on redemande
+
+     if ($sexe != "m" && $sexe != "f")
+     {
+
+        $sexe = readline ("merci d'indiquer votre sexe (tapez m ou f: ");
+
+     }
+
+ // on demande l'age de l'utilisateur    
+  $age = readline("merci d'indiquer votre age: ");
+    
+// on test les prerequis afin de savoir si on paye ou pas un impot
+
+    if()
+
+?>
