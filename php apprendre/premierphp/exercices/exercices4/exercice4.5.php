b9418f58557d2570e90a6f6c1fc256a96ea4ad8d
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-09-15 14:58:21 +0200

    plus propre

--- php apprendre/premierphp/exercices/exercices4/exercice4.5.php
+++ php apprendre/premierphp/exercices/exercices4/exercice4.5.php
@@ -1,32 +1,26 @@
 <?php
 
 // on demande le sexe a l'utilisateur
-  $sexe = readline ("merci d'indiquer votre sexe (tapez m ou f): ");
+$sexe = readline("merci d'indiquer votre sexe (tapez m ou f): ");
 
 // on test quand meme que la valeur entrée est correcte sinon on redemande
 
-     if ($sexe != "m" && $sexe != "f")
-     {
-        
-        echo "votre saisi est incorrecte\n";
-        $sexe = readline ("merci d'indiquer votre sexe (tapez m ou f: ");
+if ($sexe != "m" && $sexe != "f") {
 
-     }
+    echo "votre saisi est incorrecte\n";
+    $sexe = readline("merci d'indiquer votre sexe (tapez m ou f: ");
 
- // on demande l'age de l'utilisateur    
-  $age = readline("merci d'indiquer votre age: ");
-    
+}
+
+// on demande l'age de l'utilisateur
+$age = readline("merci d'indiquer votre age: ");
+
 // on test les prerequis afin de savoir si on paye ou pas un impot
 
-    if($age>20 && $sexe ="m" OR ($age>18 && $age<35) && $sexe = "f")
-    {
+if ($age > 20 && $sexe = "m" or ($age > 18 && $age < 35) && $sexe = "f") {
 
-        echo "vous payer un impot";
-    }
-    
-    else
-    {
-        echo "vous ne payer pas d'impot";
+    echo "vous payer un impot";
+} else {
+    echo "vous ne payer pas d'impot";
 
-    }
-?>
+}
