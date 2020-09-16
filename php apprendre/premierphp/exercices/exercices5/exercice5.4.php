155d456c92f950a9c2cbf261b820a49139e1b68b
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-09-16 15:03:37 +0200

    ok

--- php apprendre/premierphp/exercices/exercices5/exercice5.4.php
+++ php apprendre/premierphp/exercices/exercices5/exercice5.4.php
@@ -5,7 +5,7 @@
 $nb = readline("entrez un nombre: ");
 
 // on demande l'affichage de la table de multiplication entiere
-for ($i = 1; $i < 11; $i++)
+for ($i = 1; $i < 11; $i++) // on met 11 sinon la table s arrete a 9
 {
 
   echo $nb . "x" . $i . "=" . $nb*$i;
