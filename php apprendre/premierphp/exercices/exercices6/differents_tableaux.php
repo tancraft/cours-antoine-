b97a8b2a4d8ea9532c2102b43098b585395d340c
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-09-17 17:02:29 +0200

    en cours

---
+++ php apprendre/premierphp/exercices/exercices6/differents_tableaux.php
@@ -0,0 +1,18 @@
+<?php
+
+// ici nous allons creer differents tableaux
+
+$nb= readline("combien de saisies allez vous faire? ");
+
+for ($i=0; $i<=$nb; $i++);
+{
+
+ $val= readline("veulliez entrer votre saisie");
+
+ $tab[]=$val;
+
+}
+
+ echo $tab[$i];
+
+?>
