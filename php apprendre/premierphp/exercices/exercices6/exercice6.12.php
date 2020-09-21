1b5676b36445f72ef1f987176a1719628e68178e
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-09-21 12:27:03 +0200

    exo a jour

--- php apprendre/premierphp/exercices/exercices6/exercice6.12.php
+++ php apprendre/premierphp/exercices/exercices6/exercice6.12.php
@@ -4,16 +4,13 @@
 $phrase: il faut indiquer la phrase dans les parametre a ecrire pour l'utilisateur
 renvoi la valeur nombre*/
 function demandeEntier($phrase) // Demande un entier à l'utilisateur
+
 {
-    do 
-    {
-        do 
-        {
-            $nombre= readline($phrase);
-        } 
-        while (!is_numeric($nombre)); // on verifie que la chaine de caracterer ne contient que des chiffres
-    } 
-    while (!is_int($nombre * 1)); // on vérifie que le nombre est entier (pas réel)
+    do {
+        do {
+            $nombre = readline($phrase);
+        } while (!is_numeric($nombre)); // on verifie que la chaine de caracterer ne contient que des chiffres
+    } while (!is_int($nombre * 1)); // on vérifie que le nombre est entier (pas réel)
     return $nombre; //renvoi le nombre saisi
 }
 
@@ -31,55 +28,46 @@ function afficheForeach($tab)
 }
 
 // declaration des variables pour affichage du compte des valeurs positives et negatives des tableaux
-   $neg=0;
-   $pos=0;
+$neg = 0;
+$pos = 0;
 
-   $neg1=0;
-   $pos1=0;
+$neg1 = 0;
+$pos1 = 0;
 
-$tailletab= demandeEntier("merci d'entrer le nombre de valeurs a saisir: ");
+$tailletab = demandeEntier("merci d'entrer le nombre de valeurs a saisir: ");
 
-for ($i=0; $i<$tailletab; $i++) 
-{
-    $note= demandeEntier("veulliez entrer votre saisie: ");
+for ($i = 0; $i < $tailletab; $i++) {
+    $note = demandeEntier("veulliez entrer votre saisie: ");
 
-    $tab1[]=$note;// on cree le premier tableau
-    
-    $aug=$note+1;//la notation++ ne fonctionne pas, on cree cette valeur pour la suite de l algorythme car on aurait pu faire $tab2[]=$note+1
-    $tab2[]=$aug;// on cree le 2 eme tableau
-    
+    $tab1[] = $note; // on cree le premier tableau
+
+    $aug = $note + 1; //la notation++ ne fonctionne pas, on cree cette valeur pour la suite de l algorythme car on aurait pu faire $tab2[]=$note+1
+    $tab2[] = $aug; // on cree le 2 eme tableau
+
     //compte des valeurs positives et negatives du premier tableau
-    if ($note<0)
-    {    
-       $neg+=1;
+    if ($note < 0) {
+        $neg += 1;
+    } else {
+        $pos += 1;
     }
-    else
-    {
-        $pos+=1;
-    }
-     
-        //compte des valeurs positives et negatives du deuxieme tableau
-    if ($aug<0)
-    {    
-       $neg1+=1;
-    }
-    else
-    {
-        $pos1+=1;
+
+    //compte des valeurs positives et negatives du deuxieme tableau
+    if ($aug < 0) {
+        $neg1 += 1;
+    } else {
+        $pos1 += 1;
     }
 }
 
-
 //on affiche les 2 tableaux
 
- echo"le premier tableaux\n";
+echo "le premier tableaux\n";
 afficheForeach($tab1);
 
-echo"le deuxieme tableaux\n";
+echo "le deuxieme tableaux\n";
 afficheForeach($tab2);
 
 // affichage du compte des valeurs positives et negatives pour les tableaux
-echo "il y a ". $pos ." valeurs positives dans le tableau 1\n et ". $neg ." valeurs negatives\n";
+echo "il y a " . $pos . " valeurs positives dans le tableau 1\n et " . $neg . " valeurs negatives\n";
 
-echo "il y a ". $pos1 ." valeurs positives dans le tableau 2\n et ". $neg1 ." valeurs negatives\n";
-?>
+echo "il y a " . $pos1 . " valeurs positives dans le tableau 2\n et " . $neg1 . " valeurs negatives\n";
