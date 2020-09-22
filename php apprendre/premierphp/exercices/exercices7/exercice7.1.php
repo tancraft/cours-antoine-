a93b1f44a31bff62fe060b44c8d461dd976f91f7
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-09-22 17:10:21 +0200

    j'y arrive pas

--- php apprendre/premierphp/exercices/exercices7/exercice7.1.php
+++ php apprendre/premierphp/exercices/exercices7/exercice7.1.php
@@ -7,16 +7,10 @@ $taille1=demandeEntier("combien de valeur allez vous saisir? ");
 
 $tab1=creerTab($taille1);
 
-if ($tab1[0]>$tab1[1])
+if ($tab1[$i]>$tab1[1])
 {
 
     $cons=0;
-}
-else 
-{
-
-    $cons=1;
-    echo"cette suite n'est pas consecutive";
 }
 
 while ($cons==0)
