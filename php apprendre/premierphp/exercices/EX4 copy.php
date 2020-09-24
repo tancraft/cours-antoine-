2116ef8571fafcd56729d4302ce21baefdae14ba
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-09-24 10:46:28 +0200

    maj+test variables

---
+++ php apprendre/premierphp/exercices/EX4 copy.php
@@ -0,0 +1,44 @@
+<?php
+
+
+// exercice pour afficher les stats d' un avion choisi
+
+$tab1=["BOING747","AIRBUSA380","LEARJET45","DC10","CONCORDE","ANTONOV32"];
+$tab2=["BO","AB","LI","DC","CO","AN"];
+$tab3=[800,950,700,900,1400,560];
+$tab4=[10000,12000,4500,8000,16000,2500];
+
+echo "****\tSTATISTIQUES AVIONS\t****";
+$i=0;
+do
+{
+   $code=readline("Entrez le code de l'avion : ");
+  $trouver=false;
+   if($code == $tab[$i])
+   {
+     $trouver=true;
+   
+   }
+   $i++;
+}
+while(!$trouver);
+echo "il existe pas !!!"
+
+    for ($i=0;$i<count($tab2);$i++)// tri par insertion
+    {
+      $code=readline("Entrez le code de l'avion : ");
+      $trouver=false;
+
+           if ($code == $tab2[$i])// verification des valeurs les plus grandes
+           {
+             
+            $trouver=true;
+
+            echo"parametres de l'avion ";
+
+           }
+        
+
+      }
+    
+
--- php apprendre/premierphp/pendu_projet/pendu.php
+++ php apprendre/premierphp/pendu_projet/pendu.php
@@ -4,31 +4,56 @@ function afficherTableau($tab)
 {
     echo "\n";
 
-    for ( $i=0;$i<count($tab);$i++) // la fonction count renvoi la taille du tableau
+    echo "\n";
+    foreach ($tab as $elt) // le tableau est parcouru de la 1ere à la dernière case, les cases sont mises tour à tour dans $elt
     {
-        echo $tab[$i]." ";
+        echo $elt . "\t";
     }
     echo "\n";
 }
 
 /*
-      $t = array( 'B', 'O', 'N', 'J', 'O', 'U', 'R' );
-      Echo "Cette méthode doit donner B O N J O U R et ca donne : " . afficherTableau($t);
+$t = array( 'B', 'O', 'N', 'J', 'O', 'U', 'R' );
+Echo "Cette méthode doit donner B O N J O U R et ca donne : " . afficherTableau($t);
 
+afficherTableau($t);
 
-
-      afficherTableau($t);
-
-*/
-     // 2. tab[] coderMot($mot)
-// méthode qui prend un mot en paramètre d'entrée et qui renvoi un tableau de caractères contenant autant de case que de lettres dans le mot. Chacune de ces cases contient un 
+ */
+// 2. tab[] coderMot($mot)
+// méthode qui prend un mot en paramètre d'entrée et qui renvoi un tableau de caractères contenant autant de case que de lettres dans le mot. Chacune de ces cases contient un
 //Lignes à coller dans le main
 
+function coderMot($t)
+{
+
+
+    for ($i = 0; $i<count($t);$i++)
+    {
+
+      if ($i==0)
+      {
+
+        echo $t[$i]." ";
+
+      }
+      else if($i==max($t))
+      {
+         echo $t[$i];
+
+      }
+      else
+      {
+          $mid=$t["_ "];
+         echo $mid;
+
+      }
+    }
+    return($t);
+
+}
 
 $test = "bonjour";
+
+$t= str_split($test);
+
 echo "Cette méthode doit donner        et ca donne : " . afficherTableau(coderMot($t));
-
-
-
-
-?>
