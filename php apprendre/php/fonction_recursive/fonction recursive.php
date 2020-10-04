3ea9d9b3f274d2e54fe996ca7e580def0f73b855
tancraft <tancraftwork@gmail.com>
2020-10-04 16:13:09 +0200

    modif et com du code

--- php apprendre/php/fonction_recursive/fonction recursive.php
+++ php apprendre/php/fonction_recursive/fonction recursive.php
@@ -1,26 +1,26 @@
 <?php
 
-//trouver une fonction recursive sui retourne un mot
-function retournermot($mot)
+
+/*
+départ : mot
+arrivée : tom
+
+inverser un mot consiste à mettre la 1ere lettre en 1er et inverser le reste du mot
+
+ */
+
+function inverseMot($mot)//nom de la fonction
 {
-    $temp= $tab[$i];
-    $mot= $mot[(strlen($mot)-1)-$i];
-    $mot[strlen($mot-1)-$i] =$temp;
-    if ($i == strlen($mot)) 
+    $longueur = strlen($mot);//definir la variable longueur du mot
+    if ($longueur == 1)// condition d'arret quand le mot a une longueur de 1 caractere
     {
-       return $mot;
-
-    } else 
-    {
-      retournermot();
-
-
+        return $mot;// si la condition est respecter on renvoi le mot inverser
     }
+    else
+    {
+        return substr($mot, $longueur - 1).inverseMot(substr($mot,0,$longueur-1));// sinon on soustrait une lettre au mot existant et on rappelle la fonction
+    }
+
 }
 
-
-$mot="bonjour";
-
-retournermot($mot);
-
-echo $mot;
+echo inverseMot("cheval");// test d'inversement sur le mot cheval
