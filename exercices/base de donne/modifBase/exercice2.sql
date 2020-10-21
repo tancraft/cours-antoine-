277434119de94539601cae4257d20e32ea2ae8ed
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-10-21 12:25:01 +0200

    21/10/2020 12:25:00,33

--- exercices/base de donne/modifBase/exercice2.sql
+++ exercices/base de donne/modifBase/exercice2.sql
@@ -2,25 +2,37 @@ executer les 2 instructions suivantes sur la base
 ALTER TABLE commandes ADD cde_total int;
 UPDATE commandes SET cde_total = quantiteCommande * (select prixArticle from articles where articles.idArticle = commandes.idArticle)
 
-Affichez le contenu de la table commande. Qu'y a-t-il de changé dans cette table ? Comment le total de la
+Affichez le contenu de la table commande. Quy a-t-il de changé dans cette table ? Comment le total de la
 commande a-t-il été calculé ? 
-une colonne a ete ajoutee la colonne cde_total, le total de la commande a ete calculee en prenant le prix de l article de la table article 
+une colonne a ete ajoutee la colonne cde_total dans la table commandes, le total de la commande a ete calculee en prenant le prix 
+de l article de la table article et la quantitee des articles  disponible dans la table commandes
 
 Ecrire des requêtes SELECT. Vous vérifierez que le résultat de la requête correspond à votre inspection des
 tables correspondantes. 
 A)Afficher le montant le plus élevé de commande.
+SELECT MAX(`cde_total`) AS `montant max`,`nomClient`,`prenomClient` FROM `commandes` INNER JOIN `clients` ON `commandes`.`idClient` = `clients`.`idClient`
 B) Afficher le montant moyen des commandes.
+SELECT ROUND(AVG(`cde_total`),2) AS `montant moyen`FROM `commandes`
 C) Afficher le montant le plus bas de commande.
+SELECT MIN(`cde_total`) AS `montant min`,`nomClient`,`prenomClient` FROM `commandes` INNER JOIN `clients` ON `commandes`.`idClient` = `clients`.`idClient`
 D) Afficher le nombre de commandes qui ont été passées.
+SELECT COUNT(`idCommande`) AS `nombre de commandes passees` FROM `commandes`
 E) Afficher le montant moyen de commande par client
+SELECT ROUND(AVG(`cde_total`),2) AS `montant moyen par client`,`nomClient`,`prenomClient` FROM `commandes` INNER JOIN `clients` ON `commandes`.`idClient` = `clients`.`idClient` GROUP BY `clients`.`idClient`
 F) Afficher le montant le plus élevé de commande par client.
+SELECT MAX(`cde_total`) AS `montant le plus eleve`,`nomClient`,`prenomClient` FROM `commandes` INNER JOIN `clients` ON `commandes`.`idClient` = `clients`.`idClient` GROUP BY `clients`.`idClient`
 G) Afficher le nombre de commandes par client.
-H) Afficher le nombre d 'articles commandés en moyenne par client
-I) Afficher le nombre d'articles commandés en moyenne par article.
-J) Afficher le nombre total d'articles commandés par article.
-K) Afficher le nombre moyen d'articles par client et par date.
+SELECT COUNT(`idCommande`) AS `nombre de commandes`,`nomClient`,`prenomClient` FROM `commandes` INNER JOIN `clients` ON `commandes`.`idClient` = `clients`.`idClient` GROUP BY `clients`.`idClient`
+H) Afficher le nombre d articles commandés en moyenne par client
+SELECT ROUND(AVG(`quantiteCommande`),2) AS `moyenne d article commande`,`nomClient`,`prenomClient` FROM `commandes` INNER JOIN `clients` ON `commandes`.`idClient` = `clients`.`idClient` GROUP BY `clients`.`idClient`
+I) Afficher le nombre d articles commandés en moyenne par article.
+SELECT ROUND(AVG(`quantiteCommande`),2) AS `moyenne commande par article`,`descriptionArticle` FROM `commandes` INNER JOIN `articles` ON `commandes`.`idArticle` = `articles`.`idArticle` GROUP BY `articles`.`idArticle`
+J) Afficher le nombre total d articles commandés par article.
+SELECT SUM(`quantiteCommande`) AS `nombre moyen d'articles`,`descriptionArticle` FROM `commandes` INNER JOIN `articles` ON `commandes`.`idArticle` = `articles`.`idArticle` GROUP BY `commandes`.`idArticle`
+K) Afficher le nombre moyen d articles par client et par date.
+SELECT ROUND(AVG(`quantiteCommande`),2) AS `nombre moyen d'articles`,`descriptionArticle`,`nomClient`,`prenomClient` FROM `commandes` INNER JOIN `articles` INNER JOIN `clients` ON `commandes`.`idArticle` = `articles`.`idArticle` ON `commandes`.`idClient` = `articles`.`idArticle` GROUP BY `commandes`.`idArticle`
 L) Afficher le nombre de commandes par jour.
 M) Afficher le nombre de clients dans la table.
 N) Afficher le nombre de clients différents qui ont passé commande.
-O) Afficher le nombre d' articles différents qui ont été commandés.
+O) Afficher le nombre d articles différents qui ont été commandés.
 P) Afficher le nombre de jours différents de commandes
