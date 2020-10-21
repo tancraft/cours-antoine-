3f84913a6a21c91c75385319d3bcf46ffc0b9a65
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-10-21 17:25:00 +0200

    21/10/2020 17:25:00,26

--- exercices/base de donne/modifBase/exercice3.sql
+++ exercices/base de donne/modifBase/exercice3.sql
@@ -5,6 +5,9 @@ tables correspondantes.
 A)Les noms des étudiants nés avant l''étudiant « JULES LECLERCQ »
 SELECT `nomEtudiant`, `prenomEtudiant`, `dateNaissanceEtudiant` FROM `etudiants` WHERE `dateNaissanceEtudiant` <( SELECT `dateNaissanceEtudiant` FROM `etudiants` WHERE `nomEtudiant` = "LECLERCQ" AND prenomEtudiant = "Jules" )
 B) Les noms et notes des étudiants qui ont,à l''épreuve 4, une note supérieure à la moyenne de cette épreuve.
+SELECT `nomEtudiant`,`prenomEtudiant`,`note`, (SELECT AVG(`note`) FROM `avoir_note` WHERE `avoir_note`.`idEpreuve`=4) As `` FROM `avoir_note` INNER JOIN `etudiants` ON `avoir_note`.`idEtudiant`= `etudiants`.`idEtudiant`
+WHERE `avoir_note`.`idEpreuve`=4 
+AND `avoir_note`.`note`>(SELECT AVG(`note`) FROM `avoir_note` WHERE `avoir_note`.`idEpreuve`=4)
 
 C) Le nom des étudiants qui ont obtenu un 12 ou plus.
 D)Le nom des étudiants qui ont dans l''épreuve 4 une note supérieure à celle obtenue par « LUC DUPONT »(à cette même épreuve).
