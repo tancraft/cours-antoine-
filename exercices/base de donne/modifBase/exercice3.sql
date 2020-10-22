82de702142ec5e0822d8dbc5d3aa6c73e6f5b554
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-10-22 12:25:01 +0200

    22/10/2020 12:25:00,65

--- exercices/base de donne/modifBase/exercice3.sql
+++ exercices/base de donne/modifBase/exercice3.sql
@@ -3,14 +3,16 @@ tables correspondantes.
 
 
 A)Les noms des étudiants nés avant l''étudiant « JULES LECLERCQ »
-SELECT `nomEtudiant`, `prenomEtudiant`, `dateNaissanceEtudiant` FROM `etudiants` WHERE `dateNaissanceEtudiant` <( SELECT `dateNaissanceEtudiant` FROM `etudiants` WHERE `nomEtudiant` = "LECLERCQ" AND prenomEtudiant = "Jules" )
+SELECT `nomEtudiant`, `prenomEtudiant`, `dateNaissanceEtudiant` FROM `etudiants` WHERE `dateNaissanceEtudiant` <( SELECT `dateNaissanceEtudiant` FROM `etudiants` WHERE `nomEtudiant` = "LECLERCQ" AND `prenomEtudiant` = "Jules" )
 B) Les noms et notes des étudiants qui ont,à l''épreuve 4, une note supérieure à la moyenne de cette épreuve.
 SELECT `nomEtudiant`,`prenomEtudiant`,`note`, ROUND((SELECT AVG(`note`) FROM `avoir_note` WHERE `avoir_note`.`idEpreuve`=4),2) As `moyenne de l'epreuve` FROM `avoir_note` INNER JOIN `etudiants` ON `avoir_note`.`idEtudiant`= `etudiants`.`idEtudiant`
 WHERE `avoir_note`.`idEpreuve`=4 
 AND `avoir_note`.`note`>(SELECT AVG(`note`) FROM `avoir_note` WHERE `avoir_note`.`idEpreuve`=4)
 
 C) Le nom des étudiants qui ont obtenu un 12 ou plus.
+SELECT `nomEtudiant`, `prenomEtudiant`,`note` FROM `avoir_note` INNER JOIN `etudiants` ON `avoir_note`.`idEtudiant`= `etudiants`.`idEtudiant` WHERE `note`>=12 AND `idEpreuve` = 4
 D)Le nom des étudiants qui ont dans l''épreuve 4 une note supérieure à celle obtenue par « LUC DUPONT »(à cette même épreuve).
+SELECT `nomEtudiant`, `prenomEtudiant`,`note` FROM `avoir_note` INNER JOIN `etudiants` ON `avoir_note`.`idEtudiant`= `etudiants`.`idEtudiant` WHERE `avoir_note`.`idEpreuve` = 4 AND `avoir_note`.`note`>(SELECT `note` FROM `avoir_note` INNER JOIN `etudiants` ON `avoir_note`.`idEtudiant`=`etudiants`.`idEtudiant`  WHERE  `nomEtudiant` = "DUPONT" AND `prenomEtudiant` = "Luc" AND `avoir_note`.`idEpreuve` = 4 )
 E) Le nom des étudiants qui partagent une ou plusieurs notes avec « LUC DUPONT ».
 
 
